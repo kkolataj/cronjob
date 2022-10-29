9ea0e226cb493e53799bb654d28f40138038b329
CLOUDE-PC\klaud <klaudiusz.kolataj@gmail.com>
2022-10-29 18:35:16 +0200

    Added mongo db connection and routes

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -3,12 +3,33 @@
         <div>
             <h1>Vue component</h1>
             <p>Currency test {{new Date().toDateString()}}</p>
+            <h4>Items: {{item}}</h4>
+            <button type="button" class="button" @click="onClick">Click</button>
         </div>
     </main>
 </template>
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+  data() {
+    return {
+      item: null,
+      errors: []
+    }
+  },
+
+  methods:{
+    onClick: function(){
+        axios.get(`http://127.0.0.1:8000/books/cronjob`)
+        .then(response => {
+            this.item = response.data.name;
+        })
+        .catch(e => {
+            this.errors.push(e)
+        })
+    }
+  }
 }
 </script>
 
@@ -18,8 +39,14 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: #7da0b1;
+    background-color: #a7f0ff;
     height: 100vh;
     width: 100vw;
+}
+.button{
+    font-size: 20px;
+    padding: 5px 10px;
+    background-color:cadetblue;
+    cursor:pointer;
 }
 </style>
