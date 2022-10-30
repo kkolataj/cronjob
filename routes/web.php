c5f08482835273797b31810e02e34488980c533a
CLOUDE-PC\klaud <klaudiusz.kolataj@gmail.com>
2022-10-30 19:33:32 +0100

    Added custom login and register page

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,34 @@ use App\Http\Controllers\BookController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'App\Http\Controllers'], function()
+{
+    /**
+     * Home Routes
+     */
+    Route::get('/', 'HomeController@index')->name('home.index');
+
+    Route::group(['middleware' => ['guest']], function() {
+        /**
+         * Register Routes
+         */
+        Route::get('/register', 'RegisterController@show')->name('register.show');
+        Route::post('/register', 'RegisterController@register')->name('register.perform');
+
+        /**
+         * Login Routes
+         */
+        Route::get('/login', 'LoginController@show')->name('login.show');
+        Route::post('/login', 'LoginController@login')->name('login.perform');
+
+    });
+
+    Route::group(['middleware' => ['auth']], function() {
+        /**
+         * Logout Routes
+         */
+        Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
+    });
 });
 
-Route::get('books/{id}', [BookController::class, 'showItemById']);
+// Route::get('books/{id}', [BookController::class, 'showItemById']);
