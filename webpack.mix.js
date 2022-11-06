1abc9fbfa57e07d547fbbc46b10b9115c422ba1f
CLOUDE-PC\klaud <klaudiusz.kolataj@gmail.com>
2022-11-06 23:35:05 +0100

    no message

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/build/assets/.*js')
+mix.js('resources/js/app.js', 'public/build/assets/')
     .vue()
-    .postCss('resources/css/app.css', 'public/build/assets/.*css')
+    .postCss('resources/css/app.css', 'public/build/assets/')
     .browserSync('http://127.0.0.1:8000');
