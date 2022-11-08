7a2bdc215fabc31e26b2cd5b2deafd151a346ba1
Klaudiusz Kołątaj <reavbox@gmail.com>
2022-11-08 12:18:53 +0100

    Update Cors.php

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -14,7 +14,7 @@ class Cors {
     public function handle($request, Closure $next)
     {
         return $next($request)
-          ->header('Access-Control-Allow-Origin', '*') //REPLACE STAR WITH YOUR URL
+          ->header('Access-Control-Allow-Origin', 'http://207.154.235.45') //REPLACE STAR WITH YOUR URL
           ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
           ->header('Access-Control-Allow-Headers', 'content-type, authorization, x-requested-with');
     }
