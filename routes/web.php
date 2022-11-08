ef34ff9d3f6ff4a11dfbf5d06fbce5390cacb2d0
Klaudiusz Kołątaj <reavbox@gmail.com>
2022-11-08 12:13:15 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,51 +15,43 @@ use App\Http\Controllers\ConfigController;
 |
 */
 
-Route::post('user-settings', [BookController::class, 'update']);
 
-Route::get('/user-settings/{id}', [BookController::class, 'showItemById']);
+Route::group(['namespace' => 'App\Http\Controllers', 'middleware' => 'cors'], function()
+{
+    /**
+     * Home Routes
+     */
+    Route::post('user-settings', [BookController::class, 'update']);
 
-Route::get('/user-settings/latest-rate/{currency}', [BookController::class, 'getLatestRate']);
+    Route::get('/user-settings/{id}', [BookController::class, 'showItemById']);
 
-// Route::get('/', function () {
-//     return view('vue');
-// });
+    Route::get('/user-settings/latest-rate/{currency}', [BookController::class, 'getLatestRate']);
 
-Route::get('/', function () {
-    return view('welcome');
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::group(['middleware' => ['guest']], function() {
+        /**
+         * Register Routes
+         */
+        Route::get('/register', 'RegisterController@show')->name('register.show');
+        Route::post('/register', 'RegisterController@register')->name('register.perform');
+
+        /**
+         * Login Routes
+         */
+        Route::get('/login', 'LoginController@show')->name('login.show');
+        Route::post('/login', 'LoginController@login')->name('login.perform');
+
+    });
+
+    Route::group(['middleware' => ['auth']], function() {
+        /**
+         * Logout Routes
+         */
+        Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
+    });
 });
-
-
-// Route::group(['namespace' => 'App\Http\Controllers', 'middleware' => 'cors'], function()
-// {
-//     /**
-//      * Home Routes
-//      */
-//     Route::get('/', 'HomeController@index')->name('home.index');
-
-//     Route::post('/', 'HomeController@store')->name('home.store');
-
-//     Route::group(['middleware' => ['guest']], function() {
-//         /**
-//          * Register Routes
-//          */
-//         Route::get('/register', 'RegisterController@show')->name('register.show');
-//         Route::post('/register', 'RegisterController@register')->name('register.perform');
-
-//         /**
-//          * Login Routes
-//          */
-//         Route::get('/login', 'LoginController@show')->name('login.show');
-//         Route::post('/login', 'LoginController@login')->name('login.perform');
-
-//     });
-
-//     Route::group(['middleware' => ['auth']], function() {
-//         /**
-//          * Logout Routes
-//          */
-//         Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
-//     });
-// });
 
 
